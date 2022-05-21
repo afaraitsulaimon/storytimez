fb04deb0aa425a490be5799b6b59ba8f5d1d0b21
afaraitsulaimon <afaraitsulaimon@gmail.com>
2022-05-21 15:30:11 +0100

    menu done for the page

--- story-timez/src/App.js
+++ story-timez/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
-    <div className="App">
-      <p>Welcome to the story times</p>
-    </div>
+    <BrowserRouter>
+     
+      <div className="App">
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+
+      </Routes>
+      </div>
+    
+    </BrowserRouter>
   );
 }
 
---
+++ story-timez/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Menu from '../components/Menu'
+
+
+const Home = () => {
+  return (
+    <div>
+        <Menu/>
+
+        <p>Thios is the Home Page</p>
+    </div>
+  )
+}
+
+export default Home
