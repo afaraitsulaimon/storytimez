2f6167402ca2d8b7cacf8b3bab6b177079056ac2
afaraitsulaimon <afaraitsulaimon@gmail.com>
2022-06-15 12:44:04 +0100

    add upstream

---
+++ story-timez/src/pages/AllStory.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Menu from '../components/Menu'
+
+const AllStory = () => {
+  return (
+    <div>
+        <Menu/>
+        <h2 style={{ textAlign:'center' }}>All Story</h2>
+        <Footer/>
+    </div>
+  )
+}
+
+export default AllStory
